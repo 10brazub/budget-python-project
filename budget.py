fbe5ea30b6a93aec26ce2f2d6e28fc52c7187411
Brandon <bfzw3g@umsystem.edu>
2021-04-02 16:08:37 -0500

    Added functionality to view_budget function

--- budget.py
+++ budget.py
@@ -1,7 +1,6 @@
 import time 
 
-def main_menu(name):
-    print("Welcome {}".format(name))
+def main_menu():
     print()
     time.sleep(1)
     print("What would you like to do?")
@@ -42,13 +41,15 @@ def create_budget():
         except IOError as err:
             print(err)
 
+    print()
     return budget_name
 
-def budget_details(budget_name):
+def set_budget_details(budget_name):
     cont = True
     while cont == True:
         try:
             max_amount = float(input("How much will be allocated to the {} budget?\n$".format(budget_name)))
+            print()
             cont = False
         except ValueError:
             print()
@@ -57,6 +58,7 @@ def budget_details(budget_name):
     while cont == False:
         try:
             current_amount = float(input("How much has been spent towards the {} budget?\n$".format(budget_name)))
+            print()
             cont = True
 
         except ValueError:
@@ -66,7 +68,28 @@ def budget_details(budget_name):
     return max_amount, current_amount
 
 def view_budget():
-    
+    time.sleep(1)
+    for budget_name in budget_dict.keys():
+                print(f"Budget Name: {budget_name}\n")
+    while True:
+        try:
+            time.sleep(1)
+            chosen_budget_name = str(input("What budget would you like to view ?\n"))
+            if chosen_budget_name not in budget_dict:
+                raise NameError("It seems like that budget doesn't exist.\n")
+            else:
+                break
+
+        except NameError as err:
+            print(err)
+
+    print()
+    print("Locating existing budget...\n")
+    time.sleep(1)
+    print("You are now viewing the {} budget.\n".format(chosen_budget_name))
+    print("Total Spent: $", budget_dict[chosen_budget_name][1])
+    print("Total Allocated: $", budget_dict[chosen_budget_name][0])
+    print()
 
 
 
@@ -74,26 +97,31 @@ if __name__ == "__main__":
 
     user = str(input("Enter your name\n"))
     print()
+    print("Welcome {}".format(user))
     budget_dict = {}
 
     replay = True
     while replay == True:
-        choice = main_menu(user)
+        choice = main_menu()
 
         if choice == "1":
             print()
             print("Creating a new budget...")
             time.sleep(2)
             budget = create_budget()
-            max_toSpend, curr_spent = budget_details(budget)
+            max_toSpend, curr_spent = set_budget_details(budget)
             budget_dict[budget] = (max_toSpend, curr_spent)
-            print(budget_dict)
         
         elif choice == "2":
-            print("Locating existing budget...")
+            print()
+            if budget_dict:
+                view_budget()
+
+            else:
+                print("It seems like you haven't created any budgets.\n")
+                pass
         
-        end = False
-        while end == False:
+        while True:
             try:
                 replay = str(input("Do you want to continue?[Y/N]\n")).lower()
                 if replay != "y" and replay != "n":
@@ -101,11 +129,11 @@ if __name__ == "__main__":
 
                 elif replay == "n":
                     replay = False
-                    end = True
+                    break
 
                 else:
                     replay = True
-                    end = True
+                    break
 
             except ValueError as err:
                 print(err)
